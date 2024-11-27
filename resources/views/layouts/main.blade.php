b74ed64ea35a7cc0c8a3ee82380450bd20d92c5c
DarwinRG <darwinrguillermo11@gmail.com>
2024-11-27 15:31:39 +0800

    Initial commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Paciflix</title>
+
+    <link rel="stylesheet" href="/css/main.css">
+    <livewire:styles>
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body class="font-sans bg-gray-900 text-white">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{ route('movies.index') }}">Paciflix</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <ul class="navbar-nav flex flex-col md:flex-row items-center">
+                <li class="md:ml-16 mt-3 md:mt-0">
+                    <a href="{{ route('movies.index') }}" class="hover:text-gray-300">Movies</a>
+                </li>
+                <li class="md:ml-6 mt-3 md:mt-0">
+                    <a href="{{ route('tv.index') }}" class="hover:text-gray-300">TV Shows</a>
+                </li>
+            </ul>
+            <div class="flex flex-col md:flex-row items-center">
+                <livewire:search-dropdown>
+
+            </div>
+            </ul>
+        </div>
+        </div>
+    </nav>
+    @yield('content')
+    <footer class="border border-t border-gray-800">
+        <div class="container mx-auto text-sm px-4 py-6">
+            Powered by <a href="https://www.themoviedb.org/documentation/api" class="underline hover:text-gray-300">TMDb
+                API</a>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+    <livewire:scripts>
+        @yield('scripts')
+</body>
+
+</html>

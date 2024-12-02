73a7dd937552fc29392097f086ffd0912ad0f1af
DarwinRG <darwinrguillermo11@gmail.com>
2024-12-03 04:10:12 +0800

    🔧 Update database configuration options for SSL and Redis prefix handling

--- config/database.php
+++ config/database.php
@@ -58,9 +58,13 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => extension_loaded('pdo_mysql')
+                ? array_filter([
+                    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA') !== null
+                        ? resource_path(env('MYSQL_ATTR_SSL_CA'))
+                        : null,
+                ])
+                : [],
         ],
 
         'pgsql' => [
@@ -123,7 +127,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
